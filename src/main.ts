311830289554c0903a31c97311f14789dac536ca
Josue Vasquez <advinjosuev899@gmail.com>
2024-02-11 23:08:26 -0600

    body parse

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+import * as bodyParser from 'body-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const logger =  new Logger('Bootstrap');
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   app.enableCors();
 
   app.useGlobalPipes(
